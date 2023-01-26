8742bf6278402be9f60283da88f9c1e9e9da3321
Cooper LeBrun <cooperlebrun@gmail.com>
2023-01-26 16:25:03 -0500

    add environment loader

--- inv_consumer/config/environment.rb
+++ inv_consumer/config/environment.rb
@@ -8,8 +8,6 @@ paths = ['../lib', 'app'].map { File.expand_path _1, __dir__ }
 
 $LOAD_PATH.unshift(*paths)
 
-# TODO: load per environment config based on env var
-
 # FIXME: Temporary for Inventory::Update testing, need to read these from somewhere
 SHOE_STORES = ['ALDO Centre Eaton', 'ALDO Destiny USA Mall', 'ALDO Pheasant Lane Mall', 'ALDO Holyoke Mall',
                'ALDO Maine Mall', 'ALDO Crossgates Mall', 'ALDO Burlington Mall', 'ALDO Solomon Pond Mall', 'ALDO Auburn Mall', 'ALDO Waterloo Premium Outlets']
