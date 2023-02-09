46b37c5831a1760868280a7f501bb05fd87e321a
Cooper LeBrun <cooperlebrun@gmail.com>
2023-02-09 10:39:06 -0500

    Slow inventory generation

--- bin/gen_inventory.rb
+++ bin/gen_inventory.rb
@@ -5,18 +5,22 @@ require 'json'
 STDOUT.sync = true
 
 # TODO: Load these from DB
-STORE_STORES = ['ALDO Centre Eaton', 'ALDO Destiny USA Mall', 'ALDO Pheasant Lane Mall', 'ALDO Holyoke Mall', 'ALDO Maine Mall', 'ALDO Crossgates Mall', 'ALDO Burlington Mall', 'ALDO Solomon Pond Mall', 'ALDO Auburn Mall', 'ALDO Waterloo Premium Outlets']
-SHOES_MODELS = ['ADERI', 'MIRIRA', 'CAELAN', 'BUTAUD', 'SCHOOLER', 'SODANO', 'MCTYRE', 'CADAUDIA', 'RASIEN', 'WUMA', 'GRELIDIEN', 'CADEVEN', 'SEVIDE', 'ELOILLAN', 'BEODA', 'VENDOGNUS', 'ABOEN', 'ALALIWEN', 'GREG', 'BOZZA' ]
+STORE_STORES = ['ALDO Centre Eaton', 'ALDO Destiny USA Mall', 'ALDO Pheasant Lane Mall', 'ALDO Holyoke Mall',
+                'ALDO Maine Mall', 'ALDO Crossgates Mall', 'ALDO Burlington Mall', 'ALDO Solomon Pond Mall', 'ALDO Auburn Mall', 'ALDO Waterloo Premium Outlets']
+SHOES_MODELS = %w[ADERI MIRIRA CAELAN BUTAUD SCHOOLER SODANO MCTYRE CADAUDIA RASIEN WUMA
+                  GRELIDIEN CADEVEN SEVIDE ELOILLAN BEODA VENDOGNUS ABOEN ALALIWEN GREG BOZZA]
 INVENTORY = Array(0..100)
-RANDOMNESS = Array(1..3)
+# RANDOMNESS = Array(1..3)
+RANDOMNESS = Array(1..2)
 
 loop do
   RANDOMNESS.sample.times do
     puts JSON.generate({
-      store: STORE_STORES.sample,
-      model: SHOES_MODELS.sample,
-      inventory: INVENTORY.sample,
-    }, quirks_mode: true)
+                         store: STORE_STORES.sample,
+                         model: SHOES_MODELS.sample,
+                         inventory: INVENTORY.sample
+                       }, quirks_mode: true)
   end
-  sleep 1
+  # sleep 1
+  sleep 3
 end
